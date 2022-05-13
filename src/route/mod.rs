719ff17b44127d5cb81f4d29dc4b5d6a6a23e8e9
Vasanth Kumar <itsparser@gmail.com>
2022-05-13 19:53:07 +0530

    added the table

--- src/route/mod.rs
+++ src/route/mod.rs
@@ -6,6 +6,7 @@ pub(crate) mod ws;
 pub(crate) mod profile;
 pub(crate) mod case;
 pub(crate) mod auth;
+pub(crate) mod table;
 
 /// general_config - this will register all the endpoint in common route
 pub fn general_config(cfg: &mut web::ServiceConfig) {
