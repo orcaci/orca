675a119108652ca5631604213d14087c3a53642b
vasanthkumar <itsparser@gmail.com>
2022-12-07 19:48:26 +0530

    Added Initial Code into Orca

---
+++ entity/src/lib.rs
@@ -0,0 +1,19 @@
+pub use sea_orm;
+
+pub mod prelude;
+pub mod role;
+pub mod role_scope;
+pub mod user;
+pub mod user_role;
+pub mod user_session;
+
+pub mod audit_log;
+
+pub mod activity;
+pub mod group;
+pub mod profile;
+pub mod profile_data;
+pub mod test_action;
+pub mod test_case;
+pub mod test_step;
+pub mod user_group;
